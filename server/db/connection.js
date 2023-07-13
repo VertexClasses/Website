7e73736fcca7642676617577a7d8bfd2536f2abe
shivam170620 <kanhaiyakumarbansal@gmail.com>
2023-07-13 19:09:45 +0530

    first commit

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 const DB = process.env.DATABASE;
 
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+// DB Value
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     // useCreateIndex:true,
     // useFindAndModify:false
-}).then(()=>console.log("Successful"))
-.catch((err)=>{
+  })
+  .then(() => console.log("Successful"))
+  .catch((err) => {
     console.log("Unsuccessful");
-});
+  });
